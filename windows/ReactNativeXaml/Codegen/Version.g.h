6789c5853932c7e05307b93e31a16182616f5410
Will Schaller <willschaller@bemyeyes.com>
2024-05-10 18:37:48 +0200

    Update build number

--- windows/ReactNativeXaml/Codegen/Version.g.h
+++ windows/ReactNativeXaml/Codegen/Version.g.h
@@ -9,4 +9,4 @@ SOURCE WINMDS USED:
 #define VERSION_MAJOR               0
 #define VERSION_MINOR               1
 #define VERSION_REVISION            0
-#define VERSION_BUILD               2
+#define VERSION_BUILD               8
