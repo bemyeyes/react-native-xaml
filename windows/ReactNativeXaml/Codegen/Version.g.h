ab6206121c125b7339f47da5ba2f78e4096056d1
Will Schaller <willschaller@bemyeyes.com>
2024-05-10 18:09:43 +0200

    Codegen

--- windows/ReactNativeXaml/Codegen/Version.g.h
+++ windows/ReactNativeXaml/Codegen/Version.g.h
@@ -7,6 +7,6 @@ SOURCE WINMDS USED:
 **************************************************************/
 
 #define VERSION_MAJOR               0
-#define VERSION_MINOR               0
+#define VERSION_MINOR               1
 #define VERSION_REVISION            0
-#define VERSION_BUILD               78
+#define VERSION_BUILD               2
